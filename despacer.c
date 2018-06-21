cc5ea980ec3f9c8d6a8f1dfc8c752d91ebb88680
Jonathan Jacobson <40133840+j-jacobson@users.noreply.github.com>
2018-06-21 15:54:29 +0000

    Create despacer.c
    Includes numbers, deletes everything that is not a character or space.

--- despacer.c
+++ despacer.c
@@ -10,9 +10,11 @@ int main(){
 
   fgets(key, 100, stdin);
   for(i = 0, j = 0; i < sizeof(key); i++){
-    if(key[i] != ' ')
+    
+    if(isdigit(key[i]) | isalpha(key[i])){
       copy[j] = key[i];
       j++;
+    }
   }
 
   for(i = 0; i < sizeof(copy); i++)
