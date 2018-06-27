8ab6a36525e5f7362ebc2ec1e7a1d171d438a775
Jonathan Jacobson <jonjacobson715@airmail.cc>
2018-06-26 22:23:09 -0500

    most_recent.c will be moved to vegnerator.c.
    * fgets() has successfully replaced scanf().
    * spaces now supported fully

--- most_recent.c
+++ most_recent.c
@@ -1,3 +1,11 @@
+/*
+
+Title: Vigenerator v3.0
+Author: Jonathan Jacobson (jonjacobson715@airmail.cc)
+Github: https://www.github.com/j-jacobson/
+
+*/
+
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
@@ -5,18 +13,40 @@
 
 #define MAX_SZ 500
 
-int i, j, k, columns;
+int i, j, k;
+int columns;
 
 char message[100], key[100];
-char message_raw[100], key_raw[100];
+char message_raw[100], key_raw[100], columns_raw[100];
 char encrypted[100];
 char choice[1];
+
+
+/* Problems that should be fixed in the future:
+
+  * The algorithm only works for lowercase letters.
+  * Symbols are practically unsupported. I don't know how the regular cipher handles them.
+    Maybe they should just be stripped from the message like spaces are. The function isalpha() could
+    help with this.
+  * Numbers are unsupported. They should be converted and encrypted somehow. Maybe a seperate version
+    of the algorithm should be created for them, it is important information.
+  * A guy on stack exchange said that you should use strtol() instead of atoi() because it has better error handling.
+  * When decrypting, the message is too short. Always.
+
+*/
 
 int table(){
 
   do{
+
+/* Get the number of columns and convert to an integer. */
+
     printf("Type the number of columns you need: ");
-    scanf("%d", columns);
+
+    fgets(columns_raw, MAX_SZ, stdin);
+    columns = atoi(columns_raw);
+
+/* Size checking. */
     if(columns < 1)
       printf("ERROR: Too few columns.\n");
     else if(columns > 50)
@@ -24,16 +54,18 @@ int table(){
   }
   while(columns < 1 | columns > 50);
 
+/* Get the Key and remove the spaces */
+
   printf("Type your key: ");
 
-  fgets(key, MAX_SZ, stdin);
+  fgets(key_raw, MAX_SZ, stdin);
   for(i = 0, j = 0; i < strlen(key_raw); i++){
-    if(key_raw[i] == ' ')
+    if(key_raw[i] == ' ' | key_raw[i] == '\n')
       continue;
     key[j++] = key_raw[i];
   }
 
-/* print the first row, the key, offset by a space. */
+/* Print the first row, the Key, offset by a space. */
 
   for(i = 0, j = 0, printf("\n    "); i < columns; i++, j++){
     if(!(key[j]))
@@ -41,15 +73,16 @@ int table(){
     printf("%c ", key[j]);
   }
 
-  for(i = 0, printf("\n  -"); i < 20; i++)
+  for(i = 0, printf("\n  -"); i < columns; i++)
     printf("--");
   printf("\n");
 
-/* print the letter of the alphabet, a '|' character, and then the letter that should be next in that alphabet */
+/* Table making function. */
+/* Print the letter of the alphabet, a '|' character, and then the letter that should be next in that alphabet. */
 
   for(k = 0; k < 26; k++){
     printf("%c |", 'a' + k);
-    for(i = 0, j = 0; i < 20; i++, j++){
+    for(i = 0, j = 0; i < columns; i++, j++){
       if(!key[j])
         j = 0;
 
@@ -66,26 +99,31 @@ int table(){
 }
 
 
+
+/* Encryption function */
 int encrypt(){
 
   do{
+
+/* Get Message and remove the spaces. */
+
     printf("Please type your message: ");
 
-    fgets(message, MAX_SZ, stdin);
-    for(i = 0, j = 0; i < strlen(message); i++){
-      if(message[i] == ' '){
+    fgets(message_raw, MAX_SZ, stdin);
+    for(i = 0, j = 0; i < strlen(message_raw); i++){
+      if(message_raw[i] == ' ' | message_raw[i] == '\n'){
         continue;
         k++;
       }
-      message[j++] = message[i];
+      message[j++] = message_raw[i];
     }
 
-    for( ;k > 0; k--)
-      message[j++] = '\0';
+/* For dubugging. Prints the Message the algorithm recieves. */
 
-    for(i = 0; i < strlen(message); i++)
-      printf("%c", message[i]);
+    /*for(i = 0; i < strlen(message); i++)
+      printf("%c", message[i]);*/
 
+/* Length checking. */
     if(strlen(message) < 1)
       printf("ERROR: Message too short.\n");
     else if(strlen(message) > 500)
@@ -95,22 +133,29 @@ int encrypt(){
 
   do {
 
+/* Get Key and remove spaces. */
+
     printf("Please type your key: ");
 
-    fgets(key, MAX_SZ, stdin);
-    for(i = 0, j = 0; i < sizeof(key); i++){
-      if(key[i] == ' ')
+    fgets(key_raw, MAX_SZ, stdin);
+    for(i = 0, j = 0; i < sizeof(key_raw); i++){
+      if(key_raw[i] == ' ' | key_raw[i] == '\n')
         continue;
-      printf("%c", key[i]);
-      key[j++] = key[i];
+      key[j++] = key_raw[i];
     }
 
+/* Length checking. */
     if(strlen(key) < (strlen(message) / 4))
       printf("ERROR: Key too short.\n");
     if(strlen(key) > 500)
       printf("ERROR: Key too long.\n");
   }
   while((strlen(key) < (strlen(message) / 4)) | strlen(key) > 500);
+
+/* Encryption algorithm. */
+/* The distance that the message character is from 'a'
+ is added to the letter of the key for that column.
+ 1 is added because the key is not the first row of the column, just the header. */
 
   printf("Your encrypted message is: ");
 
@@ -127,18 +172,22 @@ int encrypt(){
 
 }
 
+/* Decryption function. */
 int decrypt(){
 
-/* get the message */
-/* get the key */
-
-/* algorhythm should be
-  decrypted = message - (key - a)
-*/
 do{
-    printf("Please type your message: ");
-    /*despaced(&message);*/
 
+/* Get Message and remove the spaces. */
+    printf("Please type your message: ");
+
+    fgets(message_raw, MAX_SZ, stdin);
+    for(i = 0, j = 0; i < strlen(message_raw); i++){
+      if(message_raw[i] == ' ' | message_raw[i] == '\n')
+        continue;
+    message[j++] = message_raw[i];
+    }
+
+/* Length checking. */
     if(strlen(message) < 1)
       printf("ERROR: Message too short.\n");
     else if(strlen(message) > 500)
@@ -147,15 +196,28 @@ do{
   while(strlen(message) < 1 | strlen(message) > 500);
 
   do {
-    printf("Please type your key: ");
-    /*despaced(&key);*/
 
+/* Get Key and remove the spaces. */
+    printf("Please type your key: ");
+
+    fgets(key_raw, MAX_SZ, stdin);
+    for(i = 0, j = 0; i < strlen(key_raw); i++){
+      if(key_raw[i] == ' ' | key_raw[i] == '\n')
+        continue;
+      key[j++] = key_raw[i];
+    }
+
+/* Length checking. */
     if(strlen(key) < (strlen(message) / 4))
       printf("ERROR: Key too short.\n");
     if(strlen(key) > 500)
       printf("ERROR: Key too long.\n");
   }
   while((strlen(key) < (strlen(message) / 4)) | strlen(key) > 500);
+
+/* Decryption algorithm. */
+/* The distance that the key is from 'a' is subtracted from the encrypted message, undoing the encryption.
+ 1 is subtracted due to the key not being the first row of the Tabula Recta. */
 
   printf("Your decrypted message is: ");
 
@@ -177,14 +239,16 @@ do{
 }
 
 
-
+/* Main function. Allows user to pick from options Encrypt, Decrypt, or Show Table. */
 int main(){
 
   do{
     printf("Show (T)able, (E)ncrypt message, or (D)ecrypt message?: ");
     fgets(choice, MAX_SZ, stdin);
-    /*scanf("%c", choice);*/
+
   }
+
+/* This could probably be replaced/enhanced with the lower() function. */
   while((*choice != 'T') & (*choice != 'E') & (*choice != 'D'));
 
   if((*choice == 'T') | (*choice == 't'))
